c43697930c5f217da9604a33c94c385e657e3060
Melissa Santos <m.santos9@me.com>
2017-05-03 11:13:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+document.getElementById('myBtn').addEventListener('click', function() {
+  var textarea = document.getElementById('myTextarea-doc').value.split('\n'); //array
+  var find = document.getElementById('myTextarea-words').value.split('\n'); //array
+  var re = new RegExp('['+find+']', 'g'); // RegEx
+  var replace = ''; // string
+  var newName = textarea.toString().replace(re, replace); //convert array to string and find and replace;
+ 
+  
+  document.getElementById('result').innerHTML = newName.split('\n');
+  
+  console.log(textarea); //array
+  console.log(find); //array
+  console.log(re);
+  console.log(replace); //string
+  console.log(newName);
+})
 
-function myFunction() {
-    document.getElementById("myTextarea-words").select();
-}
 
-var replaceWords = myFuction;
 
