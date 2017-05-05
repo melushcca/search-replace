69780903b868004518477bc97b745bb9e0dc35df
Melissa Santos <m.santos9@me.com>
2017-05-05 16:37:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 document.getElementById('myBtn').addEventListener('click', function() {
   var textarea = document.getElementById('myTextarea-doc').value.split('\n'); //array
   var find = document.getElementById('myTextarea-words').value.split('\n'); //array
-  var re = new RegExp('['+find+']', 'g'); // RegEx
+  var re = new RegExp([find], 'g'); // RegEx
   var replace = ''; // string
   var newName = textarea.toString().replace(re, replace); //convert array to string and find and replace;
- 
-  
-  document.getElementById('result').innerHTML = newName.split('\n');
-  
+
+  document.getElementById('result').innerHTML = newName.split(',').join('\n');
+
   console.log(textarea); //array
   console.log(find); //array
   console.log(re);
   console.log(replace); //string
   console.log(newName);
-})
+});
 
 
 
