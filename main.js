2dae01890089b23901dbc6f0a04353efa5f6a53f
Melissa Santos <m.santos9@me.com>
2017-07-04 11:38:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 document.getElementById('myBtn').addEventListener('click', function() {
-var brands = document.getElementById('myTextarea-words').value.split('\n');  
+var brands = document.getElementById('myTextarea-words').value.split('\n');
 var articles = document.getElementById('myTextarea-doc').value.split('\n');
 var newName = [];
 
 
 articles.forEach(function(article) {
   brands.forEach(function(brand) {
-    article = article.replace(brand, ' ').replace('  ', ' ');
+    article = article.replace(new RegExp('\\' + brand + '\\b', 'g'), '').replace('  ', ' ').trim();
   })
   newName.push(article);
 })
 
 document.getElementById('result').innerHTML = newName.join('\n').trim();
 })
-
-
-
